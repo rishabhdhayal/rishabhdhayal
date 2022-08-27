ce54eb09576926db96db24e63badab63cf3c51c0
rishabhdhayal <76101214+rishabhdhayal@users.noreply.github.com>
2022-08-27 12:10:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+(function()
+{
+    "use strict";
+
+    var mainCircle = document.getElementById('intro_circle');
+
+    var mCheight = mainCircle.style.height ;
+    mainCircle.style.width = mCheight+"px";
+    console.log(mCheight);
+
+})();
